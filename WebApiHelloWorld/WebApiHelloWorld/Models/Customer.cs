96807cca634dc090786141c0f565459cb84f578e
Antonio Liccardi <antonio.liccardi@blexin.com>
2018-03-20 17:51:44 +0100

    no message

---
+++ WebApiHelloWorld/WebApiHelloWorld/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiHelloWorld.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+    }
+}
---
+++ WebApiHelloWorld/WebApiHelloWorld/Models/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiHelloWorld.Models
+{
+
+    public class Rootobject
+    {
+        public Contact[] Contacts { get; set; }
+    }
+
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+
+}
